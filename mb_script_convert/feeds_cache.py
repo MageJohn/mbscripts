8aa613770689e00b3cfb9e9d690899b148c5577b
Yuri Pieters <yuri@zopatista.com>
2024-12-12 13:31:25 +0000

    Test global cache
    Includes making the implementation easier to test

--- mb_script_convert/feeds_cache.py
+++ mb_script_convert/feeds_cache.py
@@ -4,34 +4,33 @@ from os import environ
 from pathlib import Path
 from typing import Any
 
-
-class Cache[_KT, _VT](dict[_KT, _VT]):
-    def __init__(self, *args, **kwargs):
-        self.modified: bool = False
-        return super().__init__(*args, **kwargs)
-
-    def __setitem__(self, key: _KT, value: _VT, /) -> None:
-        self.modified = True
-        return super().__setitem__(key, value)
-
-
 XDG_CACHE_HOME = Path(environ.get("XDG_CACHE_HOME", Path.home() / ".cache"))
 CACHE_FILE = XDG_CACHE_HOME / "mb-scripts" / "cache.pickle"
 
 
-if CACHE_FILE.exists():
-    with CACHE_FILE.open("rb") as f:
-        cache: Cache[str, Any] = pickle.load(f)
-        cache.modified = False
-else:
-    cache = Cache()
+class Cache(dict[str, Any]):
+    def __init__(self, cache_file: str | Path):
+        self.modified: bool = False
+        self.cache_file = Path(cache_file)
+        return super().__init__()
+
+    def __setitem__(self, key: str, value: Any, /) -> None:
+        self.modified = True
+        return super().__setitem__(key, value)
+
+    def load_cache(self):
+        if self.cache_file.exists():
+            with self.cache_file.open("rb") as f:
+                cache: Cache = pickle.load(f)
+                self.update(cache)
+
+    def save_cache(self):
+        if self.modified:
+            self.cache_file.parent.mkdir(parents=True, exist_ok=True)
+            with self.cache_file.open("wb") as f:
+                pickle.dump(self, f, pickle.HIGHEST_PROTOCOL)
 
 
-def save_cache():
-    if cache.modified:
-        CACHE_FILE.parent.mkdir(parents=True, exist_ok=True)
-        with CACHE_FILE.open("wb") as f:
-            pickle.dump(cache, f, pickle.HIGHEST_PROTOCOL)
-
-
-atexit.register(save_cache)
+cache = Cache(CACHE_FILE)
+cache.load_cache()
+atexit.register(cache.save_cache)
---
+++ mb_script_convert/feeds_cache_test.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from .feeds_cache import Cache
+
+
+def test_Cache(tmp_path: Path):
+    test_cache_file = tmp_path / "cache.pickle"
+    test_cache = Cache(test_cache_file)
+
+    assert not test_cache_file.exists()
+    test_cache.save_cache()
+    assert not test_cache_file.exists()
+    test_cache["foo"] = "bar"
+    test_cache.save_cache()
+    assert test_cache_file.exists()
+
+    test_cache_2 = Cache(test_cache_file)
+    test_cache_2.load_cache()
+    assert test_cache_2.modified is False
+    assert test_cache_2["foo"] == "bar"
