6a29ee102c9fd80aad12b4216a4019e299223f6f
Yuri Pieters <yuri@zopatista.com>
2024-12-13 17:28:22 +0000

    Make breakpoints relative to font
    This seems to play better with font scaling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
   content: ["./layouts/**/*.html"],
   theme: {
+    screens: {
+      sm: "48rem", // 768px (at font size 16)
+
+      md: "64rem", // 1024px
+
+      lg: "85.375rem", // 1366px
+
+      xl: "120rem", // 1920px
+
+      "2xl": "160rem", // 2560px
+    },
     extend: {
       colors: {
         "brand-1": "#313866",
