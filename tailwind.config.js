b2aab547ed62cc3463f7b3a1e5c78b80d8820ed8
Yuri Pieters <yuri@zopatista.com>
2025-01-03 10:09:54 +0000

    Add custom styled form controls
    This is to make dark mode easier to style

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,18 @@ export default {
   theme: {
     screens: {
       sm: "48rem", // 768px (at font size 16)
-
       md: "64rem", // 1024px
-
       lg: "85.375rem", // 1366px
-
       xl: "120rem", // 1920px
-
       "2xl": "160rem", // 2560px
+    },
+    borderWidth: {
+      DEFAULT: "0.0625rem",
+      0: "0",
+      2: "0.125rem",
+      3: "0.1875rem",
+      4: "0.25rem",
+      8: "0.5rem",
     },
     extend: {},
   },
