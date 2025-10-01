20c082298f7cca1fb46a6dc36241938eb700a424
Yuri Pieters <yuri@zopatista.com>
2025-10-01 10:59:27 +0100

    Normalisation to remove a colon from series title

--- converter/mb_script_convert/normalisations.py
+++ converter/mb_script_convert/normalisations.py
@@ -5,9 +5,16 @@ from mb_script_convert.transcript import Transcript
 
 def run_all(transcript: Transcript):
     title_period(transcript)
+    series_colon(transcript)
 
 
 def title_period(transcript: Transcript):
     title = transcript.metadata.episode_title
     if title and title[-1] == ".":
         transcript.metadata.episode_title = title[:-1]
+
+
+def series_colon(transcript: Transcript):
+    series = transcript.metadata.series
+    if series and series[-1] == ":":
+        transcript.metadata.series = series[:-1]
